4f698be0a59c74e07c09ee05009d4aef2ae7f4c6
ryan <ryan@arctel.net>
2026-05-25 16:29:07 +0800

    [优化] 更新 CORS 配置以支持动态源和凭证

--- openflare_server/middleware/cors.go
+++ openflare_server/middleware/cors.go
@@ -1,12 +1,26 @@
 package middleware
 
 import (
+	"openflare/common"
+	"strings"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://openflare.vercel.app", "http://localhost:3000"}
+	config.AllowCredentials = true
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization", "X-Agent-Token", "Accept"}
+	config.AllowOriginFunc = func(origin string) bool {
+		serverAddr := strings.TrimRight(common.ServerAddress, "/")
+		if serverAddr == "" {
+			return true
+		}
+		if origin == serverAddr {
+			return true
+		}
+		return false
+	}
 	return cors.New(config)
 }
